bbc0b109873c218e1b3d51f90f9e63c5ffea7174
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-04-19 17:16:12 +0600

    Fetch API Is Done

--- js/script.js
+++ js/script.js
@@ -51,34 +51,50 @@
 
 
 // Promises
-let persons = [
-    {firstName: "Mosiur", lastName: "Rahman"},
-    {firstName: "Redwan", lastName: "Rahman"}
-];
+// let persons = [
+//     {firstName: "Mosiur", lastName: "Rahman"},
+//     {firstName: "Redwan", lastName: "Rahman"}
+// ];
 
-function createperson (person){
- let prom = new Promise(function(resolve, reject){
-    persons.push(person);
-    let error = false;
-    if(!error){
-        resolve();
-    } else{
-        reject("Error!: Somthing Wrong!");
-    }
- });
- return prom;
-};
+// function createperson (person){
+//  let prom = new Promise(function(resolve, reject){
+//     persons.push(person);
+//     let error = false;
+//     if(!error){
+//         resolve();
+//     } else{
+//         reject("Error!: Somthing Wrong!");
+//     }
+//  });
+//  return prom;
+// };
 
-function getperson () {
-    setTimeout(function(){
-        let output = " ";
-        persons.forEach(function(item){
-            output += `<li>${item.firstName} ${item.lastName}</li>`
-        });
-        document.getElementById("output").innerHTML = output;
-    }, 500);
+// function getperson () {
+//     setTimeout(function(){
+//         let output = " ";
+//         persons.forEach(function(item){
+//             output += `<li>${item.firstName} ${item.lastName}</li>`
+//         });
+//         document.getElementById("output").innerHTML = output;
+//     }, 500);
+// }
+
+// createperson({firstName: "Rony", lastName: "Shikder"}).then(getperson).catch(function(err){
+//     console.log(err);
+// });
+
+
+// Fetch API
+
+document.getElementById("get-data").addEventListener("click", getdata);
+function getdata(){
+    fetch("http://api.icndb.com/jokes/random")
+    .then(res => res.json())
+    .then(data=>{
+        // console.log(data.value.joke);
+        let joke = `<li>${data.value.joke}</li>`
+        document.getElementById("output").innerHTML = joke
+    })
+    .catch(err => console.log(err))
+
 }
-
-createperson({firstName: "Rony", lastName: "Shikder"}).then(getperson).catch(function(err){
-    console.log(err);
-});
